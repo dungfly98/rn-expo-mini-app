5b01c280bbb303eef0d8b402c6776df1be87574d
Drake Nguyen <drake.nguyen@sotatek.com>
2024-02-29 20:57:40 +0700

    Add get info device

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Device from "expo-device";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+
+      <View style={{ height: 24 }} />
+      <Button
+        title="Get Info Device"
+        onPress={() => {
+          console.log("Device: ", Device);
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
